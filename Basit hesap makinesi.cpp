3316ce7ffc6000a8a119e5ded2fe2f7af90015ec
ilker610 <75069363+ilker610@users.noreply.github.com>
2022-02-15 21:29:37 +0300

    Add files via upload

---
+++ Basit hesap makinesi.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int ilk_sayi;
+	int ikinci_sayi;
+
+	cout << "Hesap makinesine hos geldiniz.\n";
+	cout << "Lutfen ilk sayiyi giriniz:";
+	cin >> ilk_sayi;
+	cout << "Lutfen ikinci sayiyi giriniz:";
+	cin >> ikinci_sayi;
+	cout << endl;
+	
+	int toplam = ilk_sayi + ikinci_sayi;
+	int fark = ilk_sayi - ikinci_sayi;
+	float bolme = ilk_sayi / ikinci_sayi;
+	int carpma = ilk_sayi * ikinci_sayi;
+
+	cout << "Toplam:" << toplam << endl;
+	cout << "Fark:" << fark << endl;
+	cout << "Bolum:" << bolme << endl;
+	cout << "Carpim:" << carpma << endl;
+}
